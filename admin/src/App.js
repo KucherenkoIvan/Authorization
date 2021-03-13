44ddf97fcc4356fad6cb14e6dffa824367d53ced
Decan <foparu68@gmail.com>
2021-03-13 17:30:47 +0500

    Добавили обработчик на авторизацию

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Auth from './components/auth/Auth';
 
 function App() {
--- admin/src/components/auth/Auth.js
+++ admin/src/components/auth/Auth.js
@@ -4,19 +4,34 @@ import './style.scss';
 /*
 */
 export default function Auth(props){
+  const [value, setValue] = useState({
+    user_name: "",
+    user_password: ""
+  });
+
+  const onChange = e => setValue({
+    ...value,
+    [e.target.name]: e.target.value
+  })
+
+  console.log(value);
+  
   return(
-    <div>
+    <div className="auth_div">
+      <h3 className="title_auth">
+        Авторизация
+      </h3>
       <label>
         Логин
       </label>
-      <input />
-
+      <input className="form_control" type="text" name="user_name" placeholder="Ваш логин" onChange={onChange}/>
+      
       <label>
         Пароль
       </label>
-      <input />
+      <input className="form_control" type="password" name="user_password" placeholder="Ваш пароль" onChange={onChange}/>
       
-      <button>
+      <button className="btn">
         Войти
       </button>
     </div>
